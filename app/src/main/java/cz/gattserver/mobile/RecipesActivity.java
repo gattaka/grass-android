41c01aedef10648714f682f06760f31ab94eee8f
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-02-10 19:32:35 +0100

    Static task classes refactoring

--- app/src/main/java/cz/gattserver/mobile/RecipesActivity.java
+++ app/src/main/java/cz/gattserver/mobile/RecipesActivity.java
@@ -24,7 +24,66 @@ import java.util.List;
 
 public class RecipesActivity extends GrassActivity {
 
-    private String msg = "GrassAPP: ";
+    private static String msg = "GrassAPP: ";
+
+    private static class FetchTask extends URLTask {
+
+        private GrassActivity grassActivity;
+
+        public FetchTask(GrassActivity grassActivity) {
+            this.grassActivity = grassActivity;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                LinearLayout layout = grassActivity.findViewById(R.id.linearLayout);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    final JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    Button btn = new Button(grassActivity);
+                    btn.setText(jsonObject.getString("name"));
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            try {
+                                Intent intent = new Intent(grassActivity, RecipeActivity.class);
+                                intent.putExtra("id", jsonObject.getString("id"));
+                                grassActivity.startActivity(intent);
+                            } catch (JSONException e) {
+                                Log.e(msg, "Recipes detail switch", e);
+                            }
+                        }
+                    });
+                    layout.addView(btn);
+                }
+            } catch (JSONException e) {
+                Log.e(msg, "JSONArray", e);
+            }
+        }
+    }
+
+    private static class CountTask extends URLTask {
+
+        private GrassActivity grassActivity;
+
+        public CountTask(GrassActivity grassActivity) {
+            this.grassActivity = grassActivity;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            int count = Integer.parseInt(result);
+
+            FetchTask fetchTask = new FetchTask(grassActivity);
+            // TODO časem udělat opravdové stránkování
+            // http://www.gattserver.cz/ws/recipes/list?pageSize=10&page=0
+            fetchTask.execute(Config.RECIPES_LIST_RESOURCE + "?pageSize=" + count + "&page=0");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,50 +92,7 @@ public class RecipesActivity extends GrassActivity {
 
         setTitle("Recepty");
 
-        URLTask countTask = new URLTask() {
-
-            @Override
-            protected void onPostExecute(String result) {
-                super.onPostExecute(result);
-                int count = Integer.parseInt(result);
-
-                URLTask fetchTask = new URLTask() {
-
-                    @Override
-                    protected void onPostExecute(String result) {
-                        super.onPostExecute(result);
-                        try {
-                            JSONArray jsonArray = new JSONArray(result);
-                            LinearLayout layout = findViewById(R.id.linearLayout);
-
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                final JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                Button btn = new Button(RecipesActivity.this);
-                                btn.setText(jsonObject.getString("name"));
-                                btn.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        try {
-                                            Intent intent = new Intent(RecipesActivity.this, RecipeActivity.class);
-                                            intent.putExtra("id", jsonObject.getString("id"));
-                                            startActivity(intent);
-                                        } catch (JSONException e) {
-                                            Log.e(msg, "Recipes detail switch", e);
-                                        }
-                                    }
-                                });
-                                layout.addView(btn);
-                            }
-                        } catch (JSONException e) {
-                            Log.e(msg, "JSONArray", e);
-                        }
-                    }
-                };
-                // TODO časem udělat opravdové stránkování
-                // http://www.gattserver.cz/ws/recipes/list?pageSize=10&page=0
-                fetchTask.execute(Config.RECIPES_LIST_RESOURCE + "?pageSize=" + count + "&page=0");
-            }
-        };
+        CountTask countTask = new CountTask(this);
         countTask.execute(Config.RECIPES_COUNT_RESOURCE);
 
         Log.d(msg, "The onCreate() event");
