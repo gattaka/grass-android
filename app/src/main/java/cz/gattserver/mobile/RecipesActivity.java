7c0a9939ee496f621c1c530275db5a32eb29e4ee
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-02-10 22:09:14 +0100

    Lazy list view

--- app/src/main/java/cz/gattserver/mobile/RecipesActivity.java
+++ app/src/main/java/cz/gattserver/mobile/RecipesActivity.java
@@ -2,36 +2,36 @@ package cz.gattserver.mobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class RecipesActivity extends GrassActivity {
 
     private static String msg = "GrassAPP: ";
+    private static int PAGE_SIZE = 10;
 
-    private static class FetchTask extends URLTask {
+    private ArrayAdapter<String> adapter;
+
+    private static class FetchItemsTask extends URLTask {
 
         private GrassActivity grassActivity;
+        private ArrayAdapter<String> adapter;
 
-        public FetchTask(GrassActivity grassActivity) {
+        public FetchItemsTask(GrassActivity grassActivity, ArrayAdapter<String> adapter) {
             this.grassActivity = grassActivity;
+            this.adapter = adapter;
         }
 
         @Override
@@ -39,12 +39,19 @@ public class RecipesActivity extends GrassActivity {
             super.onPostExecute(result);
             try {
                 JSONArray jsonArray = new JSONArray(result);
-                LinearLayout layout = grassActivity.findViewById(R.id.linearLayout);
-
                 for (int i = 0; i < jsonArray.length(); i++) {
                     final JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String name = jsonObject.getString("name");
+                    adapter.add(name);
+                }
+                /*
+                LinearLayout layout = grassActivity.findViewById(R.id.linearLayout);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    final JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String name = jsonObject.getString("name");
+                    adapter.add(name);
                     Button btn = new Button(grassActivity);
-                    btn.setText(jsonObject.getString("name"));
+                    btn.setText(name);
                     btn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
@@ -59,29 +66,72 @@ public class RecipesActivity extends GrassActivity {
                     });
                     layout.addView(btn);
                 }
+                */
             } catch (JSONException e) {
                 Log.e(msg, "JSONArray", e);
             }
         }
     }
 
-    private static class CountTask extends URLTask {
+    private static class LazyLoaderTask extends URLTask {
 
         private GrassActivity grassActivity;
+        private ArrayAdapter<String> adapter;
+        private ListView listView;
+        private ProgressBar progressBar;
+        private int totalCount = 0;
+        private int currentPage = 0;
 
-        public CountTask(GrassActivity grassActivity) {
+        public LazyLoaderTask(GrassActivity grassActivity, ArrayAdapter<String> adapter) {
             this.grassActivity = grassActivity;
+            this.adapter = adapter;
         }
 
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            int count = Integer.parseInt(result);
+            totalCount = Integer.parseInt(result);
 
-            FetchTask fetchTask = new FetchTask(grassActivity);
-            // TODO časem udělat opravdové stránkování
-            // http://www.gattserver.cz/ws/recipes/list?pageSize=10&page=0
-            fetchTask.execute(Config.RECIPES_LIST_RESOURCE + "?pageSize=" + count + "&page=0");
+            // create the ListView
+            listView = new ListView(grassActivity);
+
+            // add a ProgressBar as ListView's footer to indicate data load
+            listView.addFooterView(progressBar = new ProgressBar(grassActivity));
+
+            // create an adapter
+            adapter = new ArrayAdapter<String>(grassActivity, android.R.layout.simple_list_item_1);
+
+            // plug the adapter to the ListView
+            listView.setAdapter(adapter);
+
+            // set the ListView as the activity's content
+            //LinearLayout layout = grassActivity.findViewById(R.id.recipesLayout);
+            //layout.addView(listView);
+            grassActivity.setContentView(listView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
+            // CRUCIAL PART !! Add the LazyLoader as the onScrollListener for the ListView.
+            listView.setOnScrollListener(new LazyLoader() {
+
+                // This method is called when the user is nearing the end of the ListView
+                // and the ListView is ready to add more items.
+                @Override
+                public void loadMore(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                    load();
+                }
+            });
+        }
+
+        private void load() {
+            Log.d(msg, "load() event");
+            // The ListView needs more data. So Fetch !!
+            if (totalCount > currentPage * PAGE_SIZE) {
+                FetchItemsTask fetchTask = new FetchItemsTask(grassActivity, adapter);
+                // http://www.gattserver.cz/ws/recipes/list?pageSize=10&page=0
+                fetchTask.execute(Config.RECIPES_LIST_RESOURCE + "?pageSize=" + PAGE_SIZE + "&page=" + currentPage);
+                currentPage++;
+            } else {
+                listView.removeFooterView(progressBar);
+            }
         }
     }
 
@@ -92,9 +142,10 @@ public class RecipesActivity extends GrassActivity {
 
         setTitle("Recepty");
 
-        CountTask countTask = new CountTask(this);
-        countTask.execute(Config.RECIPES_COUNT_RESOURCE);
+        LazyLoaderTask lazyLoaderTask = new LazyLoaderTask(this, adapter);
+        lazyLoaderTask.execute(Config.RECIPES_COUNT_RESOURCE);
 
         Log.d(msg, "The onCreate() event");
     }
+
 }
---
+++ app/src/main/java/cz/gattserver/mobile/ResponseListener.java
@@ -0,0 +1,8 @@
+package cz.gattserver.mobile;
+
+import java.util.List;
+
+public interface ResponseListener {
+
+    void onResponse(List<String> responseItems);
+}
